204f6d68ba42a58cef67fc94440181bdad286524
Akshit Bhatt <akshitbhattab@gmail.com>
2025-01-01 17:12:00 +0530

    Create Question1.cpp
    https://leetcode.com/problems/circle-and-rectangle-overlapping/

---
+++ Microsoft/Question1.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    bool checkOverlap(int radius, int xCenter, int yCenter, int x1, int y1, int x2, int y2) {
+        int xNearest = max(x1, min(xCenter, x2));
+        int yNearest = max(y1, min(yCenter, y2));
+        int distanceSquared = (xNearest - xCenter) * (xNearest - xCenter) +
+                              (yNearest - yCenter) * (yNearest - yCenter);
+        return distanceSquared <= radius * radius;
+    }
+};
